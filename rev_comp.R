177453d7c6fcaa03e2b63656afc9bc6ad9771910
Richard Ding <richard@z65-50-74-163.ips.direcpath.com>
2014-09-03 01:44:02 -0400

    delete unnecessary files

--- rev_comp.R
+++
@@ -1,9 +0,0 @@
-require(seqinr)
-data = read.fasta("rosalind_rvco (2).txt",seqonly = T)
-seqs = lapply(data,s2c)
-seqs
-
-res = sapply(lapply(seqs,rev),comp,forceToLower = F)
-
-res
-sum(seqs %in% res)
